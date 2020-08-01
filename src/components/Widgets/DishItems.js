fb3adf2a77bda9b3b03975d9ee41cac435bc8fb1
weijies <weijie_sun@sfu.ca>
2020-07-31 22:43:40 -0700

    dishes_updated&animation_updated

--- src/components/Widgets/DishItems.js
+++ src/components/Widgets/DishItems.js
@@ -6,14 +6,16 @@ const DishItems = (props) =>{
 
     const imageStyle = {
         backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.6) ), url(${props.img})`,
-        height: 200 ,
-        width: 200
+        // height: 140 ,
+        // width: 220
     }
 
     return(
         <div className='dishclass'>
             <img style={imageStyle} alt=""/>
-            <span >{props.title}</span>
+            <div>
+                <span >{props.title}</span>
+            </div>
         </div>
     )
 }
