8c6ff561225014b394c9cf200467eb65e440a0a1
OkeyDokeyYooo <873262667a@gmail.com>
2020-10-04 21:01:40 -0700

    update API file

--- src/API.js
+++ src/API.js
@@ -1,16 +1,16 @@
-const baseURL = "http://localhost:8080/api"
+const baseURL = "http://3.17.208.172/api";
 
 module.exports = {
-    signup: baseURL + '/user/register',
-    login: baseURL + '/user/login',
-    allRestaurant: baseURL + '/restaurants/',
-    detailRestaurant: baseURL + '/restaurants/',
-    allCategory: baseURL + '/category',
-    categoryRestaurant: baseURL + '/category/',
-    searchResult: baseURL + '/restaurantsName',
-    userReviews: baseURL + '/user/reviews',
-    userFavorite: baseURL + '/user/favorites',
-    userCheck: baseURL + '/user/check',
-    addFavorite: baseURL + '/user/userFavorite',
-    removeFavorite: baseURL + '/user/userUnfavorite',
-}
+  signup: baseURL + "/user/register",
+  login: baseURL + "/user/login",
+  allRestaurant: baseURL + "/restaurants/",
+  detailRestaurant: baseURL + "/restaurants/",
+  allCategory: baseURL + "/category",
+  categoryRestaurant: baseURL + "/category/",
+  searchResult: baseURL + "/restaurantsName",
+  userReviews: baseURL + "/user/reviews",
+  userFavorite: baseURL + "/user/favorites",
+  userCheck: baseURL + "/user/check",
+  addFavorite: baseURL + "/user/userFavorite",
+  removeFavorite: baseURL + "/user/userUnfavorite",
+};
